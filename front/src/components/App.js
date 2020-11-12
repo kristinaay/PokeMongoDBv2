e5b92673d98f67703a3d22dcdd3a9eddb78b5e88
Alex Moeller <amoeller.psy@gmail.com>
2020-11-12 15:55:21 -0800

    fix to create db if does not exist

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -31,9 +31,16 @@ function App() {
 
   useEffect(() => {
     const getPokemon = async () => {
+      console.log("getting Pokemon");
       try {
         const _pokemon = await fetch("/pokemon").then((res) => res.json());
-        setPokemon(_pokemon);
+        let count = Object.keys(_pokemon).length;
+        if (count === 0){
+          const _newDatabase = await fetch("/start").then((res) => res.json());
+          setPokemon(_newDatabase);
+        } else {
+          setPokemon(_pokemon);
+        }  
       } catch (err) {
         console.log("error ", err);
       }
