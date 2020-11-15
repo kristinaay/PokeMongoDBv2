aff20eeb0de081d2295295fcae28ad69112137e5
Kristina <kristina.a.yin@gmail.com>
2020-11-15 17:30:10 -0500

    changing some colors

--- front/src/components/Favorites.js
+++ front/src/components/Favorites.js
@@ -123,16 +123,6 @@ function Favorites(props) {
         </form>
       </nav>
       <ol>{renderFavorites()}</ol>
-      <br /> <br />
-      <footer>
-        Created by Alex Moeller, Ely Lam and Kristina Yin 2020{" "}
-        <img
-          src="./images/pokeball.png"
-          alt="Pokeball"
-          title="Pokeball"
-          width="20"
-        />
-      </footer>
     </div>
   );
 }
